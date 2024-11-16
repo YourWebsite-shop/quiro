ccf45790fe56750271d74f7d21f08b51efccefd1
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2024-11-16 16:25:29 +0530

    caraousel

--- src/components/aboutus/Journey.tsx
+++ src/components/aboutus/Journey.tsx
@@ -6,21 +6,19 @@ import LineButton from "../LineButton";
 
 const Journey: React.FC = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-center items-center border-2 border-[#006B66] rounded-[15px] lg:gap-40 m-4 sm:m-6 lg:m-10 p-6 sm:p-8 lg:p-12">
-      <div className="flex flex-col justify-center  items-center lg:items-start lg:w-1/2">
-        <h1 className="relative text-[16px] sm:text-[32px] lg:text-[48px] font-[600] leading-[21px] sm:leading-[38.4px] lg:leading-57.6px text-[#000000] pt-5 lg:pt-16">
-          <Image
-            src={careers}
-            alt="career"
-            className="absolute w-[61px] sm:w-[63px] lg:w-[113px] bottom-6 sm:bottom-9 lg:bottom-12 -left-6 lg:left-0"
-          />
+    <div className="flex flex-col lg:flex-row justify-between items-center border-2 border-[#006B66] rounded-[15px] lg:gap-20 m-4 sm:m-6 lg:m-10 p-6 sm:p-8 lg:p-12 h-full">
+      <div className="flex flex-col justify-center items-center lg:items-start lg:w-1/2 lg:h-[500px]">
+        <h1 className="relative text-[20px] sm:text-[32px] lg:text-[40px] font-[600] leading-[26px] sm:leading-[38.4px] lg:leading-[48px] text-[#000000]">
+          <p className="billyOhioText text-[#006B66] text-[24px] lg:text-[40px] font-[400] leading-[29px] lg:leading-[48px] sm:-translate-x-4 lg:translate-x-0 -mb-1">
+            Careers
+          </p>
           Be Part of Our Journey
         </h1>
-        <p className="text-[13px] font-[400] leading-[19.5px] text-[#000000] md:hidden lg:hidden sm:block text-center py-8">
+        <p className="text-[13px] font-[400] leading-[19.5px] text-[#000000] block sm:hidden text-center py-8">
           At Quiro, we’re transforming hospitality with impactful solutions.
           Join us to create meaningful change and shape the industry's future.
         </p>
-        <p className="text-[13px] lg:text-[20px] font-[500] leading-[19.5px] lg:leading-[30px] text-[#000000] md:block lg:block sm:hidden text-center lg:text-start py-8">
+        <p className="text-[13px] lg:text-[18px] font-[500] leading-[19.5px] lg:leading-[27px] text-[#000000] hidden sm:block text-center lg:text-start py-8">
           At Quiro, we’re not just building software – we’re transforming the
           hospitality industry with solutions that matter. If you’re passionate
           about creating meaningful change, driven by innovation, and ready to
@@ -31,8 +29,13 @@ const Journey: React.FC = () => {
           <LineButton text="See open positions" />
         </div>
       </div>
-      <div className="lg:w-1/2">
-        <Image src={journey} alt="journey" />
+      <div className="lg:w-1/2 flex justify-end h-full">
+        <Image
+          src={journey}
+          alt="journey"
+          width={1000}
+          className="w-[740px] h-full object-cover lg:-m-4 lg:h-[500px] rounded-[15px]"
+        />
       </div>
     </div>
   );
