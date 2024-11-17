2c9f72595ff751f0956a3ecfdb5794ccd485383a
kunal89204 <kunalkhandelwal108@gmail.com>
2024-11-17 14:39:46 +0530

    about us figma matched

--- src/components/aboutus/Approach.tsx
+++ src/components/aboutus/Approach.tsx
@@ -72,9 +72,9 @@ const Approach: React.FC = () => {
   };
 
   return (
-    <div className="pb-20 pt-8 lg:px-20">
+    <div className="pb-[80px] sm:pb-[100px] lg:pb-[150px] pt-[60px] lg:p-[70px] lg:px-20">
       <div className="text-center sm:text-start">
-        <h3 className="text-[20px] sm:text-[32px] pb-4 lg:text-[48px] sm:px-8 font-[600] relative">
+        <h3 className="text-[20px] sm:text-[32px] pb-[32px] sm:pb-[54px] lg:text-[48px] sm:px-8 font-[600] relative">
           <p className="text-[#006B66] billyOhioText text-[24px] sm:text-[26px] lg:text-[40px] pr-44 translate-y-2">
             From Vision to Reality
           </p>
@@ -82,64 +82,60 @@ const Approach: React.FC = () => {
         </h3>
       </div>
 
-
-{/* Mobile View below */}
-<div>
-      {/* Mobile and tab accordion */}
-      <div className="p-4 lg:hidden">
-        {/* Use defaultValue to open the first item by default */}
-        <Accordion
-          type="single"
-          collapsible
-          defaultValue="item-1"
-          className="border border-b-0 border-black"
-        >
-          {approachData.map((item) => (
-            <AccordionItem
-              key={item.id}
-              value={item.id}
-              className="border-b border-black"
-            >
-              <AccordionTrigger>
-                <div className="flex justify-between w-full px-4 sm:px-12 ">
-                  <div className="text-[#F58121] font-[600] text-[20px] ">
-                    {item.number}
+      {/* Mobile View below */}
+      <div>
+        {/* Mobile and tab accordion */}
+        <div className=" px-[20px] sm:px-[40px] lg:hidden">
+          {/* Use defaultValue to open the first item by default */}
+          <Accordion
+            type="single"
+            collapsible
+            defaultValue="item-1"
+            className="border border-b-0 border-black"
+          >
+            {approachData.map((item) => (
+              <AccordionItem
+                key={item.id}
+                value={item.id}
+                className="border-b border-black"
+              >
+                <AccordionTrigger>
+                  <div className="flex justify-between w-full px-4 sm:px-12 ">
+                    <div className="text-[#F58121] font-[600] text-[20px] sm:text-[24px] ">
+                      {item.number}
+                    </div>
+                    <div className="text-[12px] sm:text-[18px] font-[600]">
+                      {item.title}
+                    </div>
                   </div>
-                  <div className="text-[12px] sm:text-[18px] font-[600]">
-                    {item.title}
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="pt-10 sm:pt-[41px]">
+                    <div>
+                      <p className="billyOhioText text-[32px] sm:text-[36px] text-[#006B66] text-center py-6">
+                        {item.heading}
+                      </p>
+                      <p className="text-[13px] font-[500] sm:text-[18px] leading-[19.5px] sm:leading-[27px] text-center px-5 sm:px-12">
+                        {item.description}
+                      </p>
+                    </div>
+                    <div className="px-4  sm:p-0 sm:mx-6 pt-8 sm:rounded-xl sm:mt-10 sm:mb-4  overflow-hidden sm:h-[257px] flex items-center justify-center">
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        width={1000}
+                        height={1000}
+                        className="w-full h-auto object-cover object-center sm:rounded-xl"
+                      />
+                    </div>
                   </div>
-                </div>
-              </AccordionTrigger>
-              <AccordionContent>
-                <div className="pt-10">
-                  <div>
-                    <p className="billyOhioText text-[32px] sm:text-[36px] text-[#006B66] text-center py-6">
-                      {item.heading}
-                    </p>
-                    <p className="text-[13px] font-[500] sm:text-[18px] leading-[19.5px] sm:leading-[27px] text-center px-5 sm:px-12">
-                      {item.description}
-                    </p>
-                  </div>
-                  <div className="px-4 sm:p-0 sm:mx-6 pt-8 sm:rounded-xl sm:mt-10 sm:mb-4  overflow-hidden sm:h-[257px] flex items-center justify-center">
-                    <Image
-                      src={item.image}
-                      alt={item.title}
-                      width={1000}
-                      height={1000}
-                      className="w-full h-auto object-cover object-center sm:rounded-xl"
-                    />
-                  </div>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
       </div>
-    </div>
-{/* Mobile View above*/}
-
-
-    
+      {/* Mobile View above*/}
 
       {/* Desktop Accordion */}
       <div className="px-0 hidden lg:flex border border-black justify-between lg:max-w-[90vw] xl:max-w-[1376px] mx-auto overflow-hidden">
@@ -161,7 +157,9 @@ const Approach: React.FC = () => {
               {/* Rotated Title */}
               <div
                 className={`-rotate-90 absolute left-6 top-1/2 -translate-x-1/2  text-[24px] font-[600] transition-all duration-300 ${
-                  isOpen ? "w-full translate-y-[20px]" : "w-[500px] translate-y-[50px]"
+                  isOpen
+                    ? "w-full translate-y-[20px]"
+                    : "w-[500px] translate-y-[50px]"
                 }`}
               >
                 {item.title}
@@ -169,7 +167,11 @@ const Approach: React.FC = () => {
 
               {/* Content */}
               {isOpen && (
-                <div className={`py-10 px-4 opacity-100 transition-opacity duration-500 min-w-[497px] -z-20 ${isOpen? "opacity-100":"opacity-0"}`}>
+                <div
+                  className={`py-10 px-4 opacity-100 transition-opacity duration-500 min-w-[497px] -z-20 ${
+                    isOpen ? "opacity-100" : "opacity-0"
+                  }`}
+                >
                   <h5 className="text-[40px] text-[#006B66] font-[400] text-center billyOhioText">
                     {item.heading}
                   </h5>
