7c9bfbd65e4b2ad5464b7f0f75e5fb91c87dd1b4
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2024-11-22 02:23:56 +0530

    customer comps

--- src/components/customers/Faq.tsx
+++ src/components/customers/Faq.tsx
@@ -1,11 +1,134 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import accdown from "@/assets/home/accdown.png";
+import accup from "@/assets/home/accup.png";
 
-const Faq:React.FC = () => {
+const Faq: React.FC = () => {
+  const faqData = [
+    {
+      question: "What is Quiro and how can it improve my hotel experience?",
+      answer:
+        "Quiro assists in streamlining hotel operations, improving service efficiency, and enhancing the guest experience through innovative technology solutions.",
+    },
+    {
+      question: "How does Quiro make my stay more convenient?",
+      answer:
+        "Quiro employs industry-standard encryption, secure data storage, and continuous monitoring to ensure the highest level of security for user data.",
+    },
+    {
+      question: "Can I customize my experience with Quiro?",
+      answer:
+        "Quiro is designed for hotels, resorts, and other hospitality businesses looking to enhance operational efficiency and guest satisfaction.",
+    },
+    {
+      question: "What kind of support can I expect during my stay?",
+      answer:
+        "Yes, Quiro offers a free trial period to allow businesses to experience its features before making a purchase decision.",
+    },
+    {
+      question: "What kind of support can I expect during my stay?",
+      answer:
+        "The cost of Quiro depends on the size and specific needs of your business. Contact our sales team for a personalized quote.",
+    },
+    {
+      question: "What kind of support does Quiro provide?",
+      answer:
+        "Quiro offers 24/7 customer support, including live chat, email assistance, and a comprehensive knowledge base.",
+    },
+    {
+      question: "Does Quiro integrate with other hotel management software?",
+      answer:
+        "Yes, Quiro seamlessly integrates with a wide range of hotel management software systems to provide a unified experience.",
+    },
+    {
+      question: "How often is Quiro updated?",
+      answer:
+        "Quiro is regularly updated with new features, security enhancements, and performance improvements based on user feedback and industry trends.",
+    },
+    {
+      question:
+        "Can hotels customize the Quiro interface to fit their branding?",
+      answer:
+        "Yes, Quiro can be customized to match your hotel's branding, including logo placement, color schemes, and other design elements.",
+    },
+    {
+      question: "Can Quiro be used to accept payments from hotel guests?",
+      answer:
+        "Absolutely! Quiro supports secure payment processing, allowing hotels to accept payments from guests directly through the platform.",
+    },
+  ];
+
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
+  const toggleFaq = (index: number) => {
+    setExpandedIndex(expandedIndex === index ? null : index);
+  };
+
   return (
-    <div>
-      faq
-    </div>
-  )
-}
+    <div className="flex justify-center items-center  lg:pb-[142px] sm:pb-[100px] pb-[80px] mt-[80px]">
+      <div className="max-w-7xl w-full">
+        {/* Header Section */}
+        <div className="flex flex-col justify-center items-center">
+          <h1 className=" font-[600] relative text-[20px] leading-[24px] sm:text-[24px] sm:leading-[28.8px] lg:text-[48px] lg:leading-[57.6px] mx:auto max-w-1/2 text-center">
+            <span
+              className="billyOhioText text-[#006B66] absolute font-[400] text-[36px] leading-[54px] sm:px-0 lg:-left-10 lg:-top-10 sm:-left-6 left-1 -top-6  
+            sm:text-[24px] sm:leading-[36px] -translate-x-6"
+            >
+              Know More
+            </span>
+            FAQs
+          </h1>
+          <p className="font-medium text[13px] sm:text-[12px] sm:leading-[18px] text-[13px] leading-[19.5px] lg:text-[20px] lg:leading-[30px] lg:pb-[96px] sm:pb-[39px] pb-[40px] text-center lg:mt-[16px] sm:mt-[7px] mt-[11px]">
+            Here are some common questions about Quiro and our hotel management
+            software.
+          </p>
+        </div>
 
-export default Faq
+        <div className="flex flex-col items-start lg:gap-4 md:gap-4 w-full lg:pl-[383px] sm:pl-[184px]">
+          {faqData.slice(0, faqData.length / 2).map((faq, index) => (
+            <div key={index} className="pb-1 sm:pb-2 lg:pb-4 justify-center">
+              {/* Question */}
+              <div
+                className="flex items-center justify-center cursor-pointer"
+                onClick={() => toggleFaq(index)}
+              >
+                <div className="flex items-center">
+                  <div className="w-[22px] h-[22px] lg:w-[38px] lg:h-[38px] sm:w-[18px] sm:h-[18px] rounded-full aspect-square border border-black shadow-[2px_2px_0_rgba(0,0,0,1)] flex items-center m-2 p-1">
+                    {expandedIndex === index ? (
+                      <Image
+                        src={accup}
+                        alt="accup"
+                        width={1000}
+                        className="w-[10px] sm:w-[6px] lg:w-[13px] "
+                      />
+                    ) : (
+                      <Image
+                        src={accdown}
+                        alt="accdown"
+                        width={1000}
+                        className="w-[10px] sm:w-[6px]  lg:w-[13px] "
+                      />
+                    )}
+                  </div>
+                  <h2 className="lg:text-[18px] font-[500] lg:leading-[23.4px] leading-[18px] sm:text-[12px] text-[12px] sm:leading-[15.6px] break-words ">
+                    {faq.question}
+                  </h2>
+                </div>
+              </div>
+
+              {/* Answer */}
+              {expandedIndex === index && (
+                <p className="sm:text-[14px] text-[12px] font-[400] leading-[23px] ml-12 md:text-[14px] text-gray-600 lg:pl-8">
+                  {faq.answer}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
--- src/components/customers/Offerings.tsx
+++ src/components/customers/Offerings.tsx
@@ -1,11 +1,118 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import offering1 from "@/assets/customers/offerings1.png";
+import offering2 from "@/assets/customers/offerings2.png";
+import offering3 from "@/assets/customers/offerings3.png";
+import artwork from "@/assets/customers/offering1artwork.png";
 
-const Offerings:React.FC = () => {
+const Offerings: React.FC = () => {
   return (
-    <div>
-      Offerings
-    </div>
-  )
-}
+    <div className="flex flex-col items-center px-4 sm:pl-[90px] sm:pr-[75px] lg:px-16">
+      <div className="text-center mb-[30px] sm:mb-[50px] lg:mb-[100px]">
+        <p className="billyOhioText text-[#006B66] text-[16px] sm:text-[20px] lg:text-[28px] font-[400] text-start -translate-x-2 sm:-translate-x-6">
+          Offerings
+        </p>
+        <h1 className="text-[#000000] text-[20px] sm:text-[24px] sm:leading-[28.8px] lg:text-[36px] font-[600] leading-tight pb-[14px] sm:pb-[10px]">
+          Let’s dive into what we bring to the table
+        </h1>
+        <p className="text-[13px] sm:text-[11px] sm:leading-[16.5px] lg:text-[18px] font-[500]">
+          Food obviously, but much more!
+        </p>
+      </div>
 
-export default Offerings
+      {/* Section 1 */}
+      <div className="flex flex-col sm:flex-row justify-between items-center lg:items-start sm:gap-[36px] lg:gap-[101px] ">
+        <div className="relative sm:w-1/2 lg:w-2/5 flex justify-center lg:justify-end sm:px-0 sm:mb-0">
+          <Image
+            src={offering1}
+            alt="journey"
+            width={1000}
+            className="w-full max-w-[551px] lg:max-w-none h-auto rounded-[3px] lg:rounded-[10px] shadow-[4.88px_4.88px_0_rgba(0,0,0,1)] lg:shadow-[7.81px_7.81px_0_rgba(0,0,0,1)]  mb-[24px]"
+          />
+          <Image src={artwork} alt="artwork" className="absolute w-[30px]" />
+        </div>
+        <div className="sm:w-3/5 lg:w-[70%] text-center sm:text-left flex flex-col justify-center h-full">
+          <h1 className="text-[#000000] text-[16px] leading-[20.8px] font-[700] sm:text-[16px] sm:leading-[19.2px] lg:text-[20px] text-center sm:text-start lg:leading-[24px] pt-[24px] pb-[12px] sm:pt-[34px] lg:pt-[47px] sm:pb-[8px] lg:pb-[24px] w-full">
+            Convenience Redefined
+          </h1>
+          <p className="text-[#000000] pb-[20px] font-[400] text-[10px] leading-[15px] sm:text-[8px] sm:leading-[12px] lg:text-[14px] lg:leading-[21px] mb-[40px] hidden sm:block lg:hidden">
+            No more waiting or phone calls. With Quiro, guests can request
+            services, place orders, or make payments with a simple QR code scan,
+            ensuring a hassle-free and modern experience.
+          </p>
+          <p className="text-[#000000] pb-[20px] font-[500] text-[10px] leading-[15px] sm:text-[10px] sm:leading-[15px] lg:text-[14px] lg:leading-[21px] mb-[40px] block sm:hidden lg:block">
+            No more waiting or phone calls. With Quiro, guests can request
+            services, place orders, or make payments with a simple QR code scan,
+            ensuring a hassle-free and modern experience. This seamless process
+            allows staff to respond faster, improving efficiency and enhancing
+            guest satisfaction. Say hello to convenience and elevated service at
+            every step.
+          </p>
+        </div>
+      </div>
+
+      {/* Section 2 */}
+      <div className="flex flex-col sm:flex-row-reverse justify-between items-center lg:items-start sm:gap-[36px] lg:gap-[101px] ">
+        <div className="relative sm:w-1/2 lg:w-2/5 flex justify-center lg:justify-end sm:px-0 sm:mb-0">
+          <Image
+            src={offering2}
+            alt="journey"
+            width={1000}
+            className="w-full max-w-[551px] lg:max-w-none h-auto rounded-[3px] lg:rounded-[10px] shadow-[4.88px_4.88px_0_rgba(0,0,0,1)] lg:shadow-[7.81px_7.81px_0_rgba(0,0,0,1)]  mb-[24px]"
+          />
+          <Image src={artwork} alt="artwork" className="absolute w-[30px]" />
+        </div>
+        <div className="sm:w-3/5 lg:w-[70%] text-center sm:text-left flex flex-col justify-center h-full">
+          <h1 className="text-[#000000] text-[16px] leading-[20.8px] font-[700] sm:text-[16px] sm:leading-[19.2px] lg:text-[20px] text-center sm:text-start lg:leading-[24px] pt-[24px] pb-[12px] sm:pt-[34px] lg:pt-[47px] sm:pb-[8px] lg:pb-[24px] w-full">
+            Instant Service
+          </h1>
+          <p className="text-[#000000] pb-[20px] font-[400] text-[10px] leading-[15px] sm:text-[8px] sm:leading-[12px] lg:text-[14px] lg:leading-[21px] mb-[40px] hidden sm:block lg:hidden">
+            Quick responses to every request – from extra towels to room service
+            – with real-time updates for faster fulfillment. Quiro streamlines
+            communication between guests and staff, reducing delays and ensuring
+            nothing falls through the cracks.
+          </p>
+          <p className="text-[#000000] pb-[20px] font-[500] text-[10px] leading-[15px] sm:text-[10px] sm:leading-[15px] lg:text-[14px] lg:leading-[21px] mb-[40px] block sm:hidden lg:block">
+            Quick responses to every request – from extra towels to room service
+            – with real-time updates for faster fulfillment. Quiro streamlines
+            communication between guests and staff, reducing delays and ensuring
+            nothing falls through the cracks. Deliver a superior experience that
+            keeps guests coming back!
+          </p>
+        </div>
+      </div>
+
+      {/* Section 3 */}
+      <div className="flex flex-col sm:flex-row justify-between items-center lg:items-start sm:gap-[36px] lg:gap-[101px] ">
+        <div className="relative sm:w-1/2 lg:w-2/5 flex justify-center lg:justify-end sm:px-0 sm:mb-0">
+          <Image
+            src={offering3}
+            alt="journey"
+            width={1000}
+            className="w-full max-w-[551px] lg:max-w-none h-auto rounded-[3px] lg:rounded-[10px] shadow-[4.88px_4.88px_0_rgba(0,0,0,1)] lg:shadow-[7.81px_7.81px_0_rgba(0,0,0,1)]  mb-[24px]"
+          />
+          <Image src={artwork} alt="artwork" className="absolute w-[30px]" />
+        </div>
+        <div className="sm:w-3/5 lg:w-[70%] text-center sm:text-left flex flex-col justify-center h-full">
+          <h1 className="text-[#000000] text-[16px] leading-[20.8px] font-[700] sm:text-[16px] sm:leading-[19.2px] lg:text-[20px] text-center sm:text-start lg:leading-[24px] pt-[24px] pb-[12px] sm:pt-[34px] lg:pt-[47px] sm:pb-[8px] lg:pb-[24px] w-full">
+            Secure, Easy Payments
+          </h1>
+          <p className="text-[#000000] pb-[20px] font-[400] text-[10px] leading-[15px] sm:text-[8px] sm:leading-[12px] lg:text-[14px] lg:leading-[21px] mb-[40px] hidden sm:block lg:hidden">
+            Simple and secure payments with multiple options – right from your
+            device. Whether it's card payments, UPI, or mobile wallets, guests
+            can choose their preferred method with ease.
+          </p>
+          <p className="text-[#000000] pb-[20px] font-[500] text-[10px] leading-[15px] sm:text-[10px] sm:leading-[15px] lg:text-[14px] lg:leading-[21px] mb-[40px] block sm:hidden lg:block">
+            Simple and secure payments with multiple options – right from your
+            device. Whether it's card payments, UPI, or mobile wallets, guests
+            can choose their preferred method with ease. Our streamlined process
+            ensures quick transactions, enhanced security, and a hassle-free
+            payment experience for both guests and businesses.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Offerings;
