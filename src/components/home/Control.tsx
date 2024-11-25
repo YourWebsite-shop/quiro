689ef33108fdb04d9d601442e237d30053f9d74d
kunal89204 <kunalkhandelwal108@gmail.com>
2024-11-25 17:34:58 +0530

    some bugs fixed

--- src/components/home/Control.tsx
+++ src/components/home/Control.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import control1 from "@/assets/home/control/control1.png"
 import control2 from "../../assets/home/control/control2.png";
 import control3 from "../../assets/home/control/control3.png";
 import control3mobile from "../../assets/home/control/control3mobile.png";
@@ -16,7 +17,7 @@ import control7 from "../../assets/home/control/control7.png";
 
 const Control: React.FC = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 lg:pb-[165px] sm:pb-[118px] pb-[103px] xl:px-22 2xl:px-44 ">
+    <div className="flex flex-col justify-center items-center gap-2 lg:pb-[165px] sm:pb-[118px] pb-[103px] xl:px-22  ">
       <div className="flex flex-col justify-center items-center ">
         <h1 className=" font-[600] text-[20px] px-10  text-center lg:text-[40px] 3xl:text-[48px] sm:text-[24px] sm:leading-[28.8px] lg:leading-[48px] 3xl:leading-[57.6px] relative md:leading-[29px] lg:mb-[15.47px] 3xl:mb-[16px] sm:mb-[0px] mb-[11px]">
           <span className="billyOhioText text-[#006B66] px-10 absolute font-[400] text-[24px] leading-[28.8px] lg:text-[28px] 3xl:text-[36px] lg:leading-[57px] 3xl:leading-[54px] sm:text-[24px] sm:leading-[28.8px]  sm:px-10 lg:-left-6 lg:-top-10 sm:-left-6 left-3 -top-6 ">
@@ -29,17 +30,17 @@ const Control: React.FC = () => {
           and unlock more revenue
         </p>
       </div>
-      <div className="px-[24px] sm:px-[28px] lg:ml-[259px] lg:mr-[252px]   lg:px-0 3xl:w-max-[1155px]">
+      <div className="px-[24px] sm:px-[28px] lg:mx-[150px] xl:ml-[259px] xl:mr-[252px]   lg:px-0 3xl:w-max-[1155px]">
         {/*Bentou Container  */}
         <div>
           {/* first four 4 card container */}
-          <div className="flex gap-[9px] w-full">
+          <div className="flex gap-[9px] lg:gap-[17.68px] w-full">
             {/* First coloumn */}
-            <div className="flex flex-col w-[59.7%] sm:w-[68%] lg:w-[73%] gap-[9px]">
+            <div className="flex flex-col w-[59.7%] sm:w-[68%] lg:w-[73%] gap-[9px] lg:gap-[17.68px]">
               {/* Control 1 and Control 2 container - flex only on sm and above */}
-              <div className="sm:flex sm:gap-[9px]">
+              <div className="sm:flex sm:gap-[9px] lg:gap-[8.68px]">
                 {/* Control 1 mobile */}
-                <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden h-[120px] lg:h-auto min-h-[120px] sm:w-[74%]">
+                <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden h-[120px] lg:h-[184px] 3xl:h-auto min-h-[120px] sm:w-[74%]">
                   {/* Mobile image */}
                   <Image
                     src={control1mobile.src}
@@ -53,7 +54,14 @@ const Control: React.FC = () => {
                     src={control1tab.src}
                     alt="control background"
                     fill
-                    className="absolute hidden sm:block lg:rounded-[10px]  top-0 left-0 w-full h-full  z-0"
+                    className="absolute hidden sm:block lg:hidden lg:rounded-[10px]  top-0 left-0 w-full h-full  z-0"
+                  />
+                  {/* Laptop image */}
+                  <Image
+                    src={control1.src}
+                    alt="control background"
+                    fill
+                    className="absolute hidden lg:block  lg:rounded-[10px]  top-0 left-0 w-full h-full  z-0"
                   />
                   <div className="relative z-10 pl-[13px] lg:pl-[24.11px] 3xl:pl-[30px] pt-[13px] lg:pt-[25.71px] 3xl:pt-[32px]">
                     <div className="absolute right-[11px] top-[11px] lg:right-[38px] lg:top-[25px]">
@@ -69,7 +77,7 @@ const Control: React.FC = () => {
                     {/* Tab heading */}
                     <h5
                       className="
-                    text-[12px]  hidden sm:block lg:hidden font-[600] leading-[12px] text-[#F5F3ED] pb-[5px]  pr-[36px]
+                    text-[12px]  hidden sm:block lg:hidden font-[600] leading-[12px] sm:leading-[14.4px] text-[#F5F3ED] pb-[5px]  pr-[36px]
                     "
                     >
                       Order &amp; Request Management
@@ -83,7 +91,7 @@ const Control: React.FC = () => {
                 </div>
 
                 {/* Control 2 - Only visible on sm and above */}
-                <div className="hidden sm:block relative rounded-[6px] lg:rounded-[10px] overflow-hidden h-[120px] lg:h-auto  min-h-[120px] sm:w-[26%]">
+                <div className="hidden sm:block relative rounded-[6px] lg:rounded-[10px] overflow-hidden h-[120px] lg:h-[184px] 3xl:h-auto  min-h-[120px] sm:w-[26%]">
                   <Image
                     src={control2.src}
                     alt="control background"
@@ -91,7 +99,7 @@ const Control: React.FC = () => {
                     className="absolute top-0 left-0 w-full h-full object-cover z-0 rounded-[6px]"
                   />
                   <div className="relative z-10 pt-[6px] lg:pt-[10px]  pl-[4px] lg:pl-[10px] pr-[10px]">
-                    <div className="text-[8px]  lg:text-[10.45px] 3xl:text-[13px] leading-[8px] lg:leading-[10.45px] font-[500] rounded-[6px] py-[2px] px-[6px] flex items-center gap-2 bg-[#F5F3ED] lg:py-[2px] 3xl:py-[4px] lg:pr-[14px] justify-between lg:mb-[177px]">
+                    <div className="text-[8px]  lg:text-[8.45px] 3xl:text-[13px] leading-[8px] lg:leading-[10.45px] font-[500] rounded-[6px] py-[2px] px-[6px] flex items-center gap-2 bg-[#F5F3ED] lg:py-[2px] 3xl:py-[4px] lg:pr-[14px] justify-between lg:mb-[177px]">
                       Qr Based Ordering
                       <ChevronCircle variant="dark" />
                     </div>
@@ -100,7 +108,7 @@ const Control: React.FC = () => {
               </div>
 
               {/* Control 3 */}
-              <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden h-[220px] min-h-[234px] lg:h-[350px] 3xl:h-[400px]">
+              <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden h-[220px] min-h-[234px] sm:min-h-[186px] lg:h-[273px] 3xl:h-[340px]">
                 <Image
                   src={control3mobile.src}
                   alt="control background"
@@ -128,7 +136,7 @@ const Control: React.FC = () => {
                   <h5 className="text-[10px]  sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] sm:leading-[14.4px] lg:leading-[23.14px] 3xl:leading-[28.8px] leading-[12px] font-[600] pb-[5px] lg:pb-[12.06px] 3xl:pb-[15px]">
                     Menu management
                   </h5>
-                  <p className="text-[6px] sm:text-[8px] lg:text-[13px] leading-[9px] lg:leading-[19.5px] font-[500] pr-[46px] lg:pr-[175px] lg:pb-[220px]">
+                  <p className="text-[6px] sm:text-[8px] lg:text-[13px] leading-[9px] lg:leading-[19.5px] font-[500] pr-[46px] sm:pr-[155px] lg:pr-[175px] lg:pb-[220px]">
                     Update and customise your offerings effortlessly! With
                     Quiro, you can manage your menu in real time, giving guests
                     the most up-to-date options.
@@ -140,7 +148,7 @@ const Control: React.FC = () => {
             {/* Second Column */}
             <div className="w-[40.3%] sm:w-[32%] lg:w-[27%] flex flex-col gap-[9px]">
               {/* Payment Management - Full height on sm and above */}
-              <div className="relative pl-[13px] lg:pl-[24px] 3xl:pl-[30px] lg:pt-[27px] 3xl:pt-[30px] pt-[13px] rounded-[6px] lg:rounded-[10px] overflow-hidden h-[200px] sm:h-[349px] min-h-[243px] sm:min-h-[349px] lg:min-h-[590px]  3xl:h-[630px]">
+              <div className="relative pl-[13px] lg:pl-[24px] 3xl:pl-[30px] lg:pt-[27px] 3xl:pt-[30px] pt-[13px] rounded-[6px] lg:rounded-[10px] overflow-hidden h-[200px] sm:h-[349px] min-h-[243px] sm:min-h-[349px] lg:min-h-[474px]  3xl:h-[591px]">
                 <Image
                   src={control4.src}
                   alt="control background"
@@ -151,10 +159,10 @@ const Control: React.FC = () => {
                   <div className="absolute right-[8px] lg:right-[27px]  ">
                     <ChevronCircle variant="light" />
                   </div>
-                  <h5 className="text-[10px] sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] leading-[12px] lg:leading-[23.14px] 3xl:leading-[28.8px] font-[600] text-[#F5F3ED] pr-[38px] lg:pr-[108px] pb-[5px] lg:pb-[11px]">
+                  <h5 className="text-[10px] sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] leading-[12px] sm:leading-[14.4px] lg:leading-[23.14px] 3xl:leading-[28.8px] font-[600] text-[#F5F3ED] pr-[38px] lg:pr-[108px] pb-[5px] lg:pb-[11px]">
                     Payment Management
                   </h5>
-                  <p className="text-[6px] lg:text-[11px] 3xl:text-[13px] lg:leading-[16.5px] 3xl:leading-[19.5px] leading-[9px] font-[500] text-[#F5F3ED] pr-[23px]  lg:pr-[27.32px] lg:pb-[322px] 3xl:pb-[385px]">
+                  <p className="text-[6px] sm:text-[8px] lg:text-[11px] 3xl:text-[13px] sm:leading-[12px] lg:leading-[16.5px] 3xl:leading-[19.5px] leading-[9px] font-[500] text-[#F5F3ED] pr-[23px]  lg:pr-[27.32px] lg:pb-[322px] 3xl:pb-[385px]">
                     Payments made easy and secure! Our platform ensures that
                     transactions are fast, reliable, and seamless, from check-in
                     to checkout.
@@ -171,7 +179,7 @@ const Control: React.FC = () => {
                   className="absolute top-0 left-0 w-full h-full z-0 rounded-[6px]"
                 />
                 <div className="relative z-10 pt-[6px] pl-[4px] pr-[10px]">
-                  <div className="text-[8px] leading-[8px] font-[500] rounded-[6px] py-[2px] flex items-center justify-between w-full max-w-[120px]  px-[6px] bg-[#F5F3ED]">
+                  <div className="text-[8px]  leading-[8px] font-[500] rounded-[6px] py-[2px] flex items-center justify-between w-full max-w-[120px]  px-[6px] bg-[#F5F3ED]">
                     Qr Based Ordering
                     <ChevronCircle variant="dark" />
                   </div>
@@ -181,7 +189,7 @@ const Control: React.FC = () => {
           </div>
 
           {/* Second row containing single image */}
-          <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden mt-[9px] h-[150px] lg:h-auto min-h-[150px]">
+          <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden mt-[9px] lg:mt-[17.68px]  h-[150px] lg:h-[258px] 3xl:h-auto min-h-[150px]">
             <Image
               src={control5.src}
               alt="control background"
@@ -203,9 +211,9 @@ const Control: React.FC = () => {
           </div>
 
           {/* Third Row */}
-          <div className="flex pt-[9px] gap-[9px]">
+          <div className="flex pt-[9px] lg:pt-[17.68px] gap-[9px] lg:gap-[17.68px]">
             {/* Left image */}
-            <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden w-1/2 lg:h-auto h-[220px] min-h-[220px]">
+            <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden w-1/2 lg:h-[273.23px] 3xl:h-auto h-[220px] min-h-[220px]">
               <Image
                 src={control5mobile}
                 alt="control5mobile"
@@ -222,7 +230,7 @@ const Control: React.FC = () => {
                 <div className="absolute right-[11px] top-[11px] lg:right-[28px] lg:top-[22px]">
                   <ChevronCircle variant="light" />
                 </div>
-                <h5 className="text-[10px] sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] leading-[12px] lg:leading-[23.14px] 3xl:leading-[28.8px] font-[600] text-[#F5F3ED] pb-[5px] lg:pb-[15px]">
+                <h5 className="text-[10px] sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] leading-[12px] sm:leading-[14.4px] lg:leading-[23.14px] 3xl:leading-[28.8px] font-[600] text-[#F5F3ED] pb-[5px] lg:pb-[15px]">
                   Insights & Analytics
                 </h5>
                 <p className="text-[6px] sm:text-[8px] lg:text-[11px] 3xl:text-[13px] leading-[9px] sm:leading-[12px] lg:leading-[16.5px] 3xl:leading-[19.5px] font-[500] sm:font-[500] text-[#F5F3ED] pr-[22px] sm:pr-[115px]  lg:pb-[220px]">
@@ -234,7 +242,7 @@ const Control: React.FC = () => {
             </div>
 
             {/* Right image */}
-            <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden w-1/2 h-[220px] lg:h-auto min-h-[220px]">
+            <div className="relative rounded-[6px] lg:rounded-[10px] overflow-hidden w-1/2 h-[220px] lg:h-[273.23px] 3xl:h-auto min-h-[220px]">
               <Image
                 src={control6mobile}
                 alt="control5mobile"
@@ -251,12 +259,15 @@ const Control: React.FC = () => {
                 <div className="absolute right-[11px] top-[11px] lg:right-[28px] lg:top-[22px]">
                   <ChevronCircle variant="light" />
                 </div>
-                <h5 className="text-[10px] sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] leading-[12px] lg:leading-[23.14px] 3xl:leading-[28.8px] font-[600] text-[#F5F3ED] pb-[5px]">
+                <h5 className="text-[10px] sm:text-[12px] lg:text-[19.29px] 3xl:text-[24px] leading-[12px] sm:leading-[14.4px] lg:leading-[23.14px] 3xl:leading-[28.8px] font-[600] text-[#F5F3ED] pb-[5px]">
                   Real-Time Dashboard
                 </h5>
-                <p className="text-[6px] sm:text-[12px] lg:text-[13px] sm:leading-[14.4px] leading-[9px] lg:leading-[19.5px] font-[500] sm:font-[400]  text-[#F5F3ED] pr-[22px] sm:pr-[66px] lg:pt-1 lg:pb-[220px]">
+                <p className="text-[6px] sm:hidden sm:text-[8px] lg:text-[13px] leading-[9px] sm:leading-[12px] lg:leading-[19.5px] font-[500] sm:font-[400]  text-[#F5F3ED] pr-[22px] sm:pr-[66px] lg:pt-1 lg:pb-[220px]">
                   One system. One log-in. Multiple features. Get all the
                   knowledge about your business at one place.
+                </p>
+                <p className="text-[6px] hidden sm:block sm:text-[8px] lg:text-[11px] leading-[9px] sm:leading-[12px] lg:leading-[19.5px] font-[500] sm:font-[400]  text-[#F5F3ED] pr-[22px] sm:pr-[66px] lg:pt-1 lg:pb-[220px]">
+                Get instant visibility into your operations! Track active, delivered, and canceled orders, monitor request and payment statuses, and keep tabs on total earnings—all in one dynamic, real-time dashboard.
                 </p>
               </div>
             </div>
@@ -274,7 +285,7 @@ const ChevronCircle = ({
 }) => {
   return (
     <div
-      className={`w-[18px] lg:w-[38px] h-[18px] lg:h-[38px] scale-90 rounded-full ${
+      className={`w-[18px] lg:w-[35px] h-[18px] lg:h-[35px] scale-90 rounded-full ${
         variant === "light"
           ? "bg-white shadow-[3px_0_0_0_#000]"
           : "bg-black shadow-[3px_0_0_0_#fff]"
--- src/components/home/Efficiency.tsx
+++ src/components/home/Efficiency.tsx
@@ -12,12 +12,12 @@ const Efficiency: React.FC = () => {
   const efficiencyData: EfficiencyCardProps[] = [
     {
       img: Img1,
-      heading: "Optimize Your Hotel's Supply Management",
+      heading: "Optimize Your Hotel's Supply ",
       text: "Manage inventory and procurement effortlessly with our streamlined supply management solutions. Keep stock levels optimized, avoid shortages, and reduce wastage to ensure your hotel runs smoothly",
     },
     {
       img: Img2,
-      heading: "Maximize Revenue with Strategic Management",
+      heading: "Maximize Revenue with Strategic",
       text: "Unlock your hotel’s revenue potential with our data-driven management services. Track performance metrics, identify growth opportunities, and implement strategies that boost profitability",
     },
     {
@@ -70,13 +70,13 @@ const SectionHeader: React.FC<{ title: string; description: string }> = ({
   description,
 }) => (
   <div className="sm:flex   sm:gap-[27px]  lg:pb-[64px] 3xl:pb-[96] sm:pb-[0px] ">
-    <div className="relative w-full text-[#000000] sm:w-[50%] lg:w-[55%]  text-[20px] leading-[24px] text-center sm:text-start sm:text-[24px] lg:text-[48px] font-[600] sm:leading-[28.8px] lg:pl-[151px] 3xl:pl-0 lg:leading-[57.6px] pl-[45px] pr-[38px] sm:px-0">
-      {title}
+    <div className="relative w-full text-[#000000] sm:w-[50%] lg:w-[55%]  text-[20px] leading-[24px] text-center sm:text-start sm:text-[24px] lg:text-[40px] 3xl:text-[48px] font-[600] sm:leading-[28.8px] lg:pl-[151px] 3xl:pl-0 lg:leading-[48px] 3xl:leading-[57.6px] pl-[45px] pr-[38px] sm:px-0">
+      {title} 
       <span className="billyOhioText text-[#006B66] absolute font-[400] text-[24px] leading-[28.8px] lg:text-[36px] lg:leading-[54px] sm:text-[24px] sm:leading-[36px] sm:px-0 lg:-left-0 lg:-top-10 pl-[47px] sm:-left-6 left-1 -top-7 sm:-top-8 lg:pl-[122px] 3xl:pl-0">
         Efficiency
       </span>
     </div>
-    <p className="  w-full text-[#000000] text-center sm:text-start sm:w-[50%] sm:text-[11px] sm:leading-[16.5px] lg:w-[45%] lg:text-[20px] lg:leading-[30px] font-[500] text-[13px] leading-[19.5px] mt-[11px] pl-[20px] pr-[18px]  sm:py-0 sm:px-0 lg:text-base lg:pr-[120px] 3xl:pr-0">
+    <p className="  w-full text-[#000000] text-center sm:text-start sm:w-[50%] sm:text-[11px] sm:leading-[16.5px] lg:w-[45%] lg:text-[16px] 3xl:text-[20px] lg:leading-[24px] 3xl:leading-[30px] font-[500] text-[13px] leading-[19.5px] mt-[11px] pl-[20px] pr-[18px]  sm:py-0 sm:px-0 lg:text-base lg:pr-[120px] 3xl:pr-0">
       {description}
     </p>
   </div>
@@ -88,7 +88,16 @@ const EfficiencyCardList: React.FC<{ data: EfficiencyCardProps[] }> = ({
 }) => (
   <div className="flex flex-col mt-[40px] sm:mt-[50px]  lg:mt-10 gap-[25px]  sm:flex-row lg:mx-[104px] sm:gap-[10px] lg:gap-[12.47px] 3xl:gap-[20px] px-[24px] sm:px-0 3xl:mx-0">
     {data.map((item, index) => (
-      <EfficiencyCard key={index} {...item} />
+      <EfficiencyCard 
+        key={index} 
+        {...item} 
+        heading={
+          <>
+            {item.heading}
+            {index < 2 && <span className="lg:hidden inline"> Management</span>}
+          </>
+        } 
+      />
     ))}
   </div>
 );
@@ -96,7 +105,7 @@ const EfficiencyCardList: React.FC<{ data: EfficiencyCardProps[] }> = ({
 /* ---------------- Efficiency Card Component ---------------- */
 interface EfficiencyCardProps {
   img: string | StaticImageData; // Accept either a string URL or StaticImageData
-  heading: string;
+  heading: string|React.ReactNode;
   text: string;
 }
 
@@ -105,18 +114,18 @@ const EfficiencyCard: React.FC<EfficiencyCardProps> = ({
   heading,
   text,
 }) => (
-  <div className="sm:border-[1.5px] lg:border-[2.32px] lg:rounded-[15.47px] border-2  border-[#006B66] rounded-[20px] sm:rounded-[8px] w-fit mx-auto max-w-[491px] lg:max-w-screen-3xl  px-[20px] pt-[20px] pb-[40px] sm:px-[14px] lg:px-[15.48px] 3xl:px-[20px] lg:pt-[15.48px] 3xl:pt-[20px] sm:pt-[14px] sm:pb-[20px]   sm:my-0 lg:shadow-[0px_4px_14px_0px_rgba(0,107,102,0.25)]">
+  <div className="sm:border-[1.5px] lg:border-[2.32px] lg:rounded-[15.47px] border-2  border-[#006B66] rounded-[20px] sm:rounded-[8px] w-fit mx-auto max-w-[491px] lg:max-w-screen-3xl  px-[20px] pt-[20px] pb-[40px] sm:px-[14px] lg:px-[15.48px] 3xl:px-[20px] lg:pt-[15.48px] 3xl:pt-[20px] sm:pt-[14px] sm:pb-[20px] lg:pb-0   sm:my-0 lg:shadow-[0px_4px_14px_0px_rgba(0,107,102,0.25)]">
     <Image
       src={img}
       alt="efficiency img"
       width={452}
       height={311}
-      className="rounded-[10px] w-full lg:rounded-[7.74px]"
+      className="rounded-[10px] w-full  lg:rounded-[7.74px]"
     />
-    <h5 className="text-[#000000] text-[16px] leading-[20.8px] font-[600] sm:text-[12px] sm:leading-[15.6px] sm:py-3 lg:text-[24px] text-center lg:leading-[31.2px] lg:py-6 3xl:pt-[34px] 3xl:pb-[16px] pt-[25px] pb-[12px]">
+    <h5 className="text-[#000000] text-[16px] leading-[20.8px] font-[600] sm:text-[12px] sm:leading-[15.6px] sm:py-3 lg:text-[19px] 3xl:text-[24px] text-center lg:leading-[31.2px] lg:py-6 3xl:pt-[34px] 3xl:pb-[16px] pt-[25px] pb-[12px]">
       {heading}
     </h5>
-    <p className="text-[#000000]  text-center font-[400] text-[10px] leading-[15px] sm:text-[8px] sm:leading-[12px] lg:text-[16px] lg:leading-[24px] lg:pb-6 sm:pb-4">
+    <p className="text-[#000000]  text-center font-[400] text-[10px] leading-[15px] sm:text-[8px] sm:leading-[12px] lg:text-[12px]  3xl:text-[16px] lg:leading-[18px] 3xl:leading-[24px] lg:pb-[27px] sm:pb-4">
       {text}
     </p>
   </div>
